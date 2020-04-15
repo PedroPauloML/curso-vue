8dc45c81c1d6dd73b29c6a3a2cb7e9bfa37b61c8
Pedro Paulo Marques de Lima <pedropaulomarqz@gmail.com>
2020-04-15 03:20:25 -0300

    Class 19 - Challenge 01

---
+++ dom/desafio-01/app.js
@@ -0,0 +1,13 @@
+new Vue({
+  el: "#desafio",
+  data: {
+    name: "Pedro Paulo",
+    age: 20,
+    imageUrl: "https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg",
+  },
+  methods: {
+    randomNumber() {
+      return Math.random() * (1 - 0) + 0;
+    },
+  },
+});
