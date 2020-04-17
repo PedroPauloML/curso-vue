fddc0f03470fb41000570c74bd42c4d977a0d954
Pedro Paulo Marques de Lima <pedropaulomarqz@gmail.com>
2020-04-17 14:43:27 -0300

    Class 108 - Components challenge

---
+++ components/componente-desafio/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+	<v-app>
+		<app-menu />
+
+		<app-toolbar />
+
+		<v-content>
+			<app-carousel />
+		</v-content>
+
+    <app-footer/>
+	</v-app>
+</template>
+
+<script>
+import Footer from "./components/Footer";
+import Carousel from "./components/Carousel";
+import Toolbar from "./components/Toolbar";
+import Menu from "./components/Menu";
+
+export default {
+  components: {
+    "app-footer": Footer,
+    "app-carousel": Carousel,
+    "app-toolbar": Toolbar,
+    "app-menu": Menu,
+  },
+};
+</script>
