8951bdf03cef5dd7013603461e51859329f32984
Pedro Paulo Marques de Lima <pedropaulomarqz@gmail.com>
2020-04-17 13:05:56 -0300

    Class 102 - App component-intro

---
+++ components/component-intro/src/components/Counter.vue
@@ -0,0 +1,48 @@
+<template>
+  <div id="counter">
+    <button @click="decrement">-</button>
+    <span>{{ counter }}</span>
+    <button @click="increment">+</button>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      counter: 0,
+    };
+  },
+  methods: {
+    increment() {
+      this.counter++;
+    },
+    decrement() {
+      this.counter++;
+    },
+  },
+};
+</script>
+
+<style>
+#counter span {
+  border-bottom: 1px solid #ccc;
+  height: 30px;
+  padding: 5px 25px;
+}
+
+#counter button {
+  height: 30px;
+  width: 30px;
+  border-radius: 15px;
+  background-color: coral;
+  color: #fff;
+  outline: none;
+}
+#counter button:first-child {
+  margin-right: 10px;
+}
+#counter button:last-child {
+  margin-left: 10px;
+}
+</style>
