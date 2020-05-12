38561adbb32cb88967b0ac4d6772c451ba2c20c7
Pedro Paulo Marques de Lima <pedropaulomarqz@gmail.com>
2020-05-12 18:17:30 -0300

    Class 186 - Directives - Multiple directives

--- directives/diretivas-exercicios/src/App.vue
+++ directives/diretivas-exercicios/src/App.vue
@@ -9,6 +9,7 @@
 		<p v-destaque:fundo.atrasar="'lightgreen'">Usando diretiva personalizada</p>
 		<hr>
 		<p v-detaque-local:fundo.atrasar="'lightgray'">Usando diretiva personalizada</p>
+		<p v-detaque-local:fundo.atrasar.alternar="'lightgray'">Usando diretiva personalizada</p>
 	</div>
 </template>
 
@@ -17,16 +18,29 @@ export default {
   directives: {
     "detaque-local": {
       bind(el, binding, vnode) {
-        // el.style.backgroundColor = "lightgreen";
+        const aplicarCor = (cor) => {
+          if (binding.arg === "fundo") {
+            el.style.backgroundColor = cor;
+          } else {
+            el.style.color = cor;
+          }
+        };
         let atraso = 0;
 
         if (binding.modifiers["atrasar"]) atraso = 1500;
 
+        const cor1 = binding.value;
+        const cor2 = "purple";
+        let corAtual = cor1;
+
         setTimeout(() => {
-          if (binding.arg === "fundo") {
-            el.style.backgroundColor = binding.value;
+          if (binding.modifiers["alternar"]) {
+            setInterval(() => {
+              corAtual = corAtual === cor1 ? cor2 : cor1;
+              aplicarCor(corAtual);
+            }, 500);
           } else {
-            el.style.color = binding.value;
+            aplicarCor(corAtual);
           }
         }, atraso);
       },
